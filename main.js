235cdf6a758a6a63fec0fdae14292f0b8cb667a6
Seabird15 <danielalizama.a@gmail.com>
2023-01-04 18:47:45 +0100

    cards

--- main.js
+++ main.js
@@ -1,8 +1,43 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu')
 
+const burguerIcon = document.querySelector('.menu');
+const menuMobile = document.querySelector('.mobile-menu');
+
+const menuCarrito = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
+
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burguerIcon.addEventListener('click', toggleMobileMenu);
+menuCarrito.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
-    desktopMenu.classList.remove('.inactive')
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive')
+    }
+
+    desktopMenu.classList.toggle('inactive')
 }
+
+function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive')
+    }
+    
+    menuMobile.classList.toggle('inactive')
+} 
+
+function toggleCarritoAside() {
+    const isMobileMenuClosed = menuMobile.classList.contains('inactive')
+
+    if(!isMobileMenuClosed){
+        menuMobile.classList.add('inactive')
+    }
+
+    aside.classList.toggle('inactive')
+} 
